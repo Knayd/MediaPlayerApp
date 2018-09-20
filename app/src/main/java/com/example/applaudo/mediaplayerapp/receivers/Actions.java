ce0e5006caf864659b0fcee575505993200f6107
Deryan Argueta <deryan.hndz@gmail.com>
2018-09-20 17:49:33 -0600

    Added Service to play music  and Broadcast Receiver

---
+++ app/src/main/java/com/example/applaudo/mediaplayerapp/receivers/Actions.java
@@ -0,0 +1,4 @@
+package com.example.applaudo.mediaplayerapp.receivers;
+
+public class Actions {
+}
---
+++ app/src/main/java/com/example/applaudo/mediaplayerapp/services/PlayerService.java
@@ -0,0 +1,4 @@
+package com.example.applaudo.mediaplayerapp.services;
+
+public class PlayerService {
+}
